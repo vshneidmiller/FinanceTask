ed2beadf07a0913847ceda65b1865e5eed3366e1
Viacheslav Shneidmiller <slyke@ukr.net>
2018-08-19 23:31:22 +0300

    minor fix

--- Finance/Program.cs
+++ Finance/Program.cs
@@ -15,37 +15,7 @@ namespace Finance
     {
         static void Main(string[] args)
         {
-
-            IWebDriver driver = new ChromeDriver();
-            HomePage home = new HomePage(driver);
-            Menu menu = new Menu(driver);
-
-            home.GoTo();
-            driver.Manage().Window.Maximize();
-
-            menu.Market.Click();
-            string actualMarketTitle = driver.Title;
-
-            menu.Nbu.Click();
-            string actualNbuTitle = driver.Title;
-
-            menu.Fuel.Click();
-            string actualFuelTitle = driver.Title;
-
-            menu.Converter.Click();
-            string actualConverterTitle = driver.Title;
-
-            menu.Main.Click();
-            string actualMainTitle = driver.Title;
-
-            Assert.AreEqual(actualMainTitle, Config.PageElements.Titles.mainTitle);
-            Assert.AreEqual(actualMarketTitle, Config.PageElements.Titles.marketTitle);
-            Assert.AreEqual(actualNbuTitle, Config.PageElements.Titles.nbuTitle);
-            Assert.AreEqual(actualConverterTitle, Config.PageElements.Titles.converterTitle);
-            Assert.AreEqual(actualMainTitle, Config.PageElements.Titles.mainTitle);
-
-            driver.Quit();
-
+                       
         }
     }
 }
